1318b2b8a7f8e1dd8fc45039eb0d20ededb5a4e5
Ben Bangert <100193+bbangert@users.noreply.github.com>
2024-09-25 19:22:49 +0000

    chore: elixir project basics

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Megahal.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Meghal is a port of the MegaHAL chatbot to Elixir."
+
   def project do
     [
       app: :megahal,
-      version: "0.1.0",
+      version: @version,
+      name: "Megahal",
+      description: @description,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -21,8 +27,22 @@ defmodule Megahal.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:typed_struct, "~> 0.3.0"},
+      {:sooth, "~> 0.3.2"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+
+      # Dev/test dependencies
+      {:stream_data, "~> 1.1", only: :test},
+      {:excoveralls, "~> 0.18.2", only: :test},
+      {:ex_doc, "~> 0.34.2", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Unlicense"],
+      maintainers: ["Ben Bangert"],
+      links: %{"GitHub" => "https://github.com/bbangert/sooth"}
+    }
   end
 end
